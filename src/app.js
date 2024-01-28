025bf3b56b6e1314ca57a49591e93f510e933d92
Pravin Kori <pravinkori@protonmail.ch>
2024-01-28 21:58:44 +0530

    refactor: Rendering Pet component with dynamic data.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 // Pet component definition
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Luna"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Havanese"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
     ]);
 };
 
@@ -11,9 +11,21 @@ const Pet = () => {
 const App = () => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, "Adopt me!"),
-        React.createElement(Pet), // Rendering Pet component
-        React.createElement(Pet),
-        React.createElement(Pet),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese",
+        }), // Rendering Pet component
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel",
+        }),
+        React.createElement(Pet, {
+            name: "Doink",
+            animal: "Cat",
+            breed: "Mixed",
+        }),
     ]);
 };
 
