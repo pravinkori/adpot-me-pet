8e533b7ffeaf0d3a92978e7824958b58fc6f71da
Pravin Kori <pravinkori@protonmail.ch>
2024-01-29 23:57:52 +0530

    feat: Add animal selection dropdown in the component
    This commit introduces a new section in the component containing a dropdown for selecting an animal. The dropdown is associated with the 'animal' state, and its options are dynamically generated from the ANIMALS array.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("");
     return (
         <div className="search-params">
             <form>
@@ -14,6 +17,24 @@ const SearchParams = () => {
                         placeholder="Enter location"
                     />
                 </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        value={animal}
+                        onChange={(e) => {
+                            setAnimal(e.target.value);
+                        }}
+                    >
+                        <option />
+                        {ANIMALS.map((animal) => (
+                            <option value={animal} key={animal}>
+                                {animal}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+
                 <button>Submit</button>
             </form>
         </div>
