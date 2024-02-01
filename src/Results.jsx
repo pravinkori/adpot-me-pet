19a7b942bdceab1d41aa6fbe0f043c9ff5a21ad2
Pravin Kori <pravinkori@protonmail.ch>
2024-02-01 22:09:00 +0530

    refactor: Implement Results component
    This commit implements 'Results' component and its functionality. The component renders a list of pet information using the 'Pet' component, or displays a message stating "No Pets Found" if the list is empty.

---
+++ src/Results.jsx
@@ -0,0 +1,25 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+    return (
+        <div className="search">
+            {!pets.lenght ? (
+                <h1>No Pets Found</h1>
+            ) : (
+                pets.map((pet) => (
+                    <Pet
+                        key={pet.id}
+                        name={pet.name}
+                        animal={pet.animal}
+                        breed={pet.breed}
+                        images={pet.images}
+                        location={`${pet.city}, ${pet.state}`}
+                        id={pet.id}
+                    />
+                ))
+            )}
+        </div>
+    );
+};
+
+export default Results;
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -85,14 +85,7 @@ const SearchParams = () => {
                 <button>Submit</button>
             </form>
 
-            {pets.map((pet) => (
-                <Pet
-                    name={pet.name}
-                    animal={pet.animal}
-                    breed={pet.breed}
-                    key={pet.id}
-                />
-            ))}
+            <Results pets={pets} />
         </div>
     );
 };
