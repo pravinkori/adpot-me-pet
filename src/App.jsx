514f953b6328b48b50f48edb5939f08f0db9d57f
Pravin Kori <pravinkori@protonmail.ch>
2024-02-06 20:24:33 +0530

    feat: Integrate react-query for data fetching and caching in the App component
    This commit enhances the 'App' component by incorporating 'QueryClientProvider'
    from '@tanstack/react-query' to provide a context for managing data fetching and caching.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity,
+        },
+    },
+});
 
 // App component definition
 const App = () => {
     return (
         <BrowserRouter>
-            <div>
-                <header>
-                    <Link to={"/"}>Adopt Me!</Link>
-                </header>
-                <Routes>
-                    <Route path="/details/:id" element={<Details />} />
-                    <Route path="/" element={<SearchParams />} />
-                </Routes>
-            </div>
+            <QueryClientProvider client={queryClient}>
+                <div>
+                    <header>
+                        <Link to={"/"}>Adopt Me!</Link>
+                    </header>
+                    <Routes>
+                        <Route path="/details/:id" element={<Details />} />
+                        <Route path="/" element={<SearchParams />} />
+                    </Routes>
+                </div>
+            </QueryClientProvider>
         </BrowserRouter>
     );
 };
