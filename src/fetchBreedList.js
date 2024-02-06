2374b05062766aa85bfd333644b15838d776825d
Pravin Kori <pravinkori@protonmail.ch>
2024-02-06 23:03:49 +0530

    feat: Implement fetchBreedList function for fetching pet breeds from the API
    This commit introduces the 'fetchBreedList' function, which asynchronously
    a list of pet breeds from the API based on the provided 'animal' parameter.
    The function guards against invalid input by returning an empty array if 'animal'
    is not provided. It throws an error if the API response is not successful.
    Otherwise, it parses the JSON response and returns the breed list.

---
+++ src/fetchBreedList.js
@@ -0,0 +1,24 @@
+// Asynchronous function to fetch a list of pet breeds based on the provided animal
+async function fetchBreedList({ queryKey }) {
+    // Extract the animal from the queryKey
+    const animal = queryKey[1];
+
+    // If animal is not provided, return an empty array
+    if (!animal) return [];
+
+    // Fetch data from the API based on the provided animal
+    const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    );
+
+    // Check if the response from the API is not ok
+    if (!res.ok) {
+        // If the response is not ok, throw an error
+        throw new Error(`breeds ${animal} fetch not ok`);
+    }
+
+    // Parse the JSON response and return it
+    return res.json();
+}
+
+export default fetchBreedList;
