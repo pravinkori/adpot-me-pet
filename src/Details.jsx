cdc16fcf6c6aa7fbf1f129ec6f2432c2b451cde7
Pravin Kori <pravinkori@protonmail.ch>
2024-02-08 18:16:36 +0530

    chore: Add comments to Details component

--- src/Details.jsx
+++ src/Details.jsx
@@ -4,8 +4,11 @@ import fetchPet from "./fetchPet";
 
 const Details = () => {
     const { id } = useParams();
+
+    // Using useQuery hook to fetch data from the API using fetchPet function
     const results = useQuery(["details", id], fetchPet);
 
+    // If data is still loading, display a loading indicator
     if (results.isLoading) {
         return (
             <div className="loading-pane">
@@ -14,8 +17,10 @@ const Details = () => {
         );
     }
 
+    // Once data is loaded, extract the pet information from the results
     const pet = results.data.pets[0];
 
+    // Display the details of the pet
     return (
         <div className="details">
             <div>
